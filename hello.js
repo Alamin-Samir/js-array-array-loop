b04b05cf8c67c0ddae99bd0702f2ab48f0e1f227
samir <sa.samir213002019@gmail.com>
2026-05-18 07:11:21 +0600

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,33 @@
+function add(a,b){
+
+    console.log(a+b)
+}
+add (4,9);
+
+
+
+let x=17;
+let y=27;
+
+if(x>y){
+
+    console.log("x is big number");
+}
+    
+    else{
+        console.log("y is big number");
+    }
+
+
+    let fruits=["apple", "banana", "orange"]
+
+    console.log(fruits);
+
+
+    console.log(fruits[0]);
+
+    for (let i=0; i<fruits.length; i++)
+
+
+
+        console.log(fruits[i]);
